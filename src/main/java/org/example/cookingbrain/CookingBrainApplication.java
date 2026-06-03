58e6c2ba6af3549cc94da1f69e9806c9a4994c33
GUSTAVO CARDOSO GUARESI <gustavo_guaresi@estudante.sesisenai.org.br>
2026-06-03 19:53:57 -0300

    Criação da estrutura inicial de pastas

---
+++ src/main/java/org/example/cookingbrain/CookingBrainApplication.java
@@ -0,0 +1,13 @@
+package org.example.cookingbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingBrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingBrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cookingbrain/CookingBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cookingbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingBrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

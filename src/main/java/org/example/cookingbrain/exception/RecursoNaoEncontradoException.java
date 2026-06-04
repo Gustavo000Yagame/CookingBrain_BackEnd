612106d034e5d741b2541b1b0e6ee63f001ef877
VITOR DE FARIAS DE PIERI <vitor_pieri@estudante.sesisenai.org.br>
2026-06-03 21:07:32 -0300

    message

---
+++ src/main/java/org/example/cookingbrain/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package org.example.cookingbrain.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/example/cookingbrain/exception/t.java
+++
@@ -1,4 +0,0 @@
-package org.example.cookingbrain.exception;
-
-public class t {
-}
